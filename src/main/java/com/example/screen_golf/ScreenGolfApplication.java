8e5a7811cb0e4778023f4755621b33e90ab750ad
taewoocode <taewoocode@gmail.com>
2025-04-22 16:30:07 +0900

    first init

---
+++ src/main/java/com/example/screen_golf/ScreenGolfApplication.java
@@ -0,0 +1,13 @@
+package com.example.screen_golf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenGolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenGolfApplication.class, args);
+	}
+
+}
