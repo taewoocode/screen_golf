80d603b207dc46be7666afdf716db3e76f0f57c7
taewoocode <taewoocode@gmail.com>
2025-04-24 06:00:33 +0900

    GF-2[feat]: UserRole, UserStatus 추가

---
+++ src/main/java/com/example/screen_golf/user/domain/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.screen_golf.user.domain;
+
+public enum UserRole {
+    USER, ADMIN
+} 
---
+++ src/main/java/com/example/screen_golf/user/domain/UserStatus.java
@@ -0,0 +1,7 @@
+package com.example.screen_golf.user.domain;
+
+public enum UserStatus {
+    ACTIVE,     // 활성
+    INACTIVE,   // 비활성
+    DELETED     // 삭제
+} 
