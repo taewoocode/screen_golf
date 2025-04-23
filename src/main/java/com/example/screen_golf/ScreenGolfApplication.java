5a4e51ac1b1c1714adb73af09deee685cad778a3
taewoocode <taewoocode@gmail.com>
2025-04-24 06:05:40 +0900

    GF-2[feat]: 그래들 추가

--- src/main/java/com/example/screen_golf/ScreenGolfApplication.java
+++ src/main/java/com/example/screen_golf/ScreenGolfApplication.java
@@ -2,12 +2,12 @@ package com.example.screen_golf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ScreenGolfApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenGolfApplication.class, args);
 	}
-
 }
