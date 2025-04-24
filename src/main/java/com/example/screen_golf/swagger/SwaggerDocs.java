fda7e3bb0f4c661a768e022460d61f86c5307f0f
taewoocode <taewoocode@gmail.com>
2025-04-24 17:32:29 +0900

    GF-2[feat]: 회원정보 스웨거 추가

--- src/main/java/com/example/screen_golf/swagger/SwaggerDocs.java
+++ src/main/java/com/example/screen_golf/swagger/SwaggerDocs.java
@@ -1,6 +1,9 @@
 package com.example.screen_golf.swagger;
 
 public class SwaggerDocs {
-    public static final String SUMMARY_USER_SIGNUP = "회원가입";
-    public static final String DESCRIPTION_USER_SIGNUP = "이메일, 비밀번호, 이름, 전화번호, 프로필 이미지를 입력하여 회원가입을 진행합니다.";
+	public static final String SUMMARY_USER_SIGNUP = "회원가입";
+	public static final String DESCRIPTION_USER_SIGNUP = "이메일, 비밀번호, 이름, 전화번호, 프로필 이미지를 입력하여 회원가입을 진행합니다.";
+
+	public static final String SUMMARY_USER_INFO = "ID로 회원조회";
+	public static final String DESCRIPTION_USER_INFO = "ID로 회원정보를 조회합니다.";
 } 
