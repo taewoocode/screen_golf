993f0014bcfad9030527625f906bf0a8bb0f6036
taewoocode <taewoocode@gmail.com>
2025-04-25 01:07:16 +0900

    GF-2[feat]: RoomStatus, Type 추가

---
+++ src/main/java/com/example/screen_golf/room/domain/RoomStatus.java
@@ -0,0 +1,8 @@
+package com.example.screen_golf.room.domain;
+
+public enum RoomStatus {
+    AVAILABLE,    // 사용 가능
+    RESERVED,     // 예약됨
+    IN_USE,       // 사용 중
+    MAINTENANCE   // 점검 중
+} 
---
+++ src/main/java/com/example/screen_golf/room/domain/RoomType.java
@@ -0,0 +1,7 @@
+package com.example.screen_golf.room.domain;
+
+public enum RoomType {
+    STANDARD,    // 일반
+    PREMIUM,     // 프리미엄
+    VIP          // VIP
+} 
