636eccb95d66116903fb21f23e32f26cf9004180
taewoocode <taewoocode@gmail.com>
2025-04-24 22:07:11 +0900

    GF-2[feat]: 예약 값 검증 예외처리 핸들러 추가

---
+++ src/main/java/com/example/screen_golf/exception/reservation/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.screen_golf.exception.reservation;
+
+public class ResourceNotFoundException extends RuntimeException {
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
