e15ac116779107057ff9905303d52fd45f858836
taewoocode <taewoocode@gmail.com>
2025-04-24 22:08:21 +0900

    GF-2[feat]: Reservation 관련 전역 예약처리 추가

--- src/main/java/com/example/screen_golf/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/screen_golf/exception/GlobalExceptionHandler.java
@@ -5,6 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.screen_golf.exception.reservation.ReservationConflictException;
+import com.example.screen_golf.exception.reservation.ReservationValidationException;
+import com.example.screen_golf.exception.reservation.ResourceNotFoundException;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -22,4 +26,27 @@ public class GlobalExceptionHandler {
 		log.error("서버 오류 발생: {}", e.getMessage(), e);
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 	}
-} 
+
+	/**=========================================================================
+	 * 							Reservation 관련
+	 * =========================================================================
+	 */
+
+	@ExceptionHandler(ReservationValidationException.class)
+	public ResponseEntity<String> handleReservationValidationException(ReservationValidationException e) {
+		log.error("예약 유효성 검사 실패: {}", e.getMessage(), e);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+	}
+
+	@ExceptionHandler(ReservationConflictException.class)
+	public ResponseEntity<String> handleReservationConflictException(ReservationConflictException e) {
+		log.error("예약 충돌 발생: {}", e.getMessage(), e);
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+		log.error("리소스 조회 실패: {}", e.getMessage(), e);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+	}
+}
