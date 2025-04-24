94bd13f2a0ab88cd112c135ffed98966fd527ede
taewoocode <taewoocode@gmail.com>
2025-04-24 17:32:16 +0900

    GF-2[feat]: 컨트롤러 회원정보 조회 추가

--- src/main/java/com/example/screen_golf/user/controller/UserController.java
+++ src/main/java/com/example/screen_golf/user/controller/UserController.java
@@ -11,6 +11,7 @@ import com.example.screen_golf.user.domain.User;
 import com.example.screen_golf.user.service.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -26,5 +27,14 @@ public class UserController {
 	@PostMapping("/signup")
 	public ResponseEntity<User.UserSignUpResponse> signUp(@RequestBody User.UserSignUpRequest request) {
 		return ResponseEntity.ok(userService.registerUser(request));
+	}
+
+	@Operation(summary = SwaggerDocs.SUMMARY_USER_INFO,
+		description = SwaggerDocs.DESCRIPTION_USER_INFO)
+	@PostMapping("/info")
+	public ResponseEntity<User.UserInfoResponse> getUserInfo(
+		@Parameter(description = "조회할 사용자 ID", required = true)
+		@RequestBody User.UserInfoRequest request) {
+		return ResponseEntity.ok(userService.findUser(request));
 	}
 } 
