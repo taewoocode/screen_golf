dd1f36ea2542a3de4199cd2e728db70acc3f22b1
taewoocode <taewoocode@gmail.com>
2025-04-26 01:29:09 +0900

    GF-3[feat]: 쿠폰서비스 테스트 코드 작성

--- src/test/java/com/example/screen_golf/coupon/service/UserCouponServiceImplTest.java
+++ src/test/java/com/example/screen_golf/coupon/service/UserCouponServiceImplTest.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+성import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.screen_golf.coupon.domain.UserCoupon;
+import com.example.screen_golf.coupon.repository.UserCouponRepository;
 import com.example.screen_golf.user.domain.User;
 import com.example.screen_golf.user.domain.UserRole;
 import com.example.screen_golf.user.domain.UserStatus;
@@ -16,9 +18,13 @@ import com.example.screen_golf.user.domain.UserStatus;
 @SpringBootTest
 class UserCouponServiceImplTest {
 
+	@Autowired
+	private UserCouponRepository userCouponRepository;
+
 	@Test
 	@DisplayName("사용자가_쿠폰을_보유하고_있으면_쿠폰_목록을_반환해야_한다")
 	void 사용자가_쿠폰을_보유하고_있으면_쿠폰_목록을_반환해야_한다() throws Exception {
+		// Given
 		User testUser = User.builder()
 			.email("testEmail")
 			.password("test")
@@ -33,7 +39,7 @@ class UserCouponServiceImplTest {
 			.name("testCouponName1")
 			.validFrom(LocalDateTime.now())
 			.validTo(LocalDateTime.now().plusDays(10))
-			.user(testUser)
+			.user(testUser)  // Coupon의 user 필드 설정
 			.build();
 
 		UserCoupon testCoupon2 = UserCoupon.builder()
@@ -41,18 +47,19 @@ class UserCouponServiceImplTest {
 			.name("testCouponName2")
 			.validFrom(LocalDateTime.now())
 			.validTo(LocalDateTime.now().plusDays(10))
-			.user(testUser)
+			.user(testUser)  // Coupon의 user 필드 설정
 			.build();
 
 		testUser.getUserCoupons().add(testCoupon1);
 		testUser.getUserCoupons().add(testCoupon2);
 
+		// When
 		List<UserCoupon> userCoupons = testUser.getUserCoupons();
 
+		// Then
 		Assertions.assertThat(userCoupons)
 			.isNotNull()
 			.hasSize(2)
 			.contains(testCoupon1, testCoupon2);
 	}
-
 }
