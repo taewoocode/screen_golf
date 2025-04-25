13e9a061bf5545e36660d8ceb3fb7f4f066179f7
taewoocode <taewoocode@gmail.com>
2025-04-25 22:13:07 +0900

    GF-3[feat]: Payment 결제 요청 정책 추가

--- src/main/java/com/example/screen_golf/reservation/config/DiscountPolicyConfig.java
+++ src/main/java/com/example/screen_golf/reservation/config/DiscountPolicyConfig.java
@@ -2,19 +2,22 @@ package com.example.screen_golf.reservation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.example.screen_golf.reservation.service.DiscountPolicy;
+import com.example.screen_golf.reservation.service.FixedAmountCountPolicy;
 import com.example.screen_golf.reservation.service.RateAmountCountPolicy;
 
 @Configuration
 public class DiscountPolicyConfig {
 
-	// @Bean
-	// public DiscountPolicy fixedAmountDiscountPolicy() {
-	// 	return new FixedAmountCountPolicy();
-	// }
+	@Bean
+	public DiscountPolicy fixedAmountDiscountPolicy() {
+		return new FixedAmountCountPolicy();
+	}
 
 	@Bean
+	@Primary
 	public DiscountPolicy rateAmountDiscountPolicy() {
 		return new RateAmountCountPolicy();
 	}
