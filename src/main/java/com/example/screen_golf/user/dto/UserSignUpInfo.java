4c7967736cdb5c632b00f0145681514cd8cd0886
taewoocode <taewoocode@gmail.com>
2025-04-29 00:46:41 +0900

    GF-4[feat]: User 로그인 정보 DTO 분리

---
+++ src/main/java/com/example/screen_golf/user/dto/UserSignUpInfo.java
@@ -0,0 +1,33 @@
+package com.example.screen_golf.user.dto;
+
+import com.example.screen_golf.user.domain.UserRole;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class UserSignUpInfo {
+
+	/** ########## 회원가입 DTO ########## **/
+	@Getter
+	@NoArgsConstructor
+	public static class UserSignUpRequest {
+		private String email;
+		private String password;
+		private String name;
+		private String phone;
+		private String profileImage;
+	}
+
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	public static class UserSignUpResponse {
+		private Long userId;
+		private String email;
+		private String name;
+		private UserRole role;
+	}
+}
