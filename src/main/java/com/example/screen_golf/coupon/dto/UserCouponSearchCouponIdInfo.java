410f3a2b2c0ca5732fa3885103e2218d756ca3ad
taewoocode <taewoocode@gmail.com>
2025-04-29 17:25:54 +0900

    GF-5[feat]: UserCouponService 쿠폰ID로 쿠폰의 정보 dto 추가

---
+++ src/main/java/com/example/screen_golf/coupon/dto/UserCouponSearchCouponIdInfo.java
@@ -0,0 +1,33 @@
+package com.example.screen_golf.coupon.dto;
+
+import java.time.LocalDateTime;
+
+import com.example.screen_golf.coupon.domain.CouponStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 쿠폰ID로 쿠폰의 정보를 조회한다.
+ */
+public class UserCouponSearchCouponIdInfo {
+
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	@Getter
+	public static class UserCouponSearchCouponIdResponse {
+		private Long id;
+		private Long userId;
+		private String couponCode;
+		private String name;
+		private Integer discountAmount;
+		private LocalDateTime validFrom;
+		private LocalDateTime validTo;
+		private CouponStatus status;
+		private LocalDateTime createdAt;
+		private LocalDateTime updatedAt;
+	}
+}
