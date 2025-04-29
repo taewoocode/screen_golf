80344f5b56aa0c321aac80604866f625ca1a11ab
taewoocode <taewoocode@gmail.com>
2025-04-29 20:32:13 +0900

    GF-5[feat]:  이용가능한 Room 조회 DTO 추가

---
+++ src/main/java/com/example/screen_golf/room/dto/AvailableRoomInfo.java
@@ -0,0 +1,40 @@
+package com.example.screen_golf.room.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import com.example.screen_golf.room.domain.RoomStatus;
+import com.example.screen_golf.room.domain.RoomType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class AvailableRoomInfo {
+
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	@Getter
+	public static class AvailableRoomRequest {
+		private LocalDate reservationDate;
+		private LocalTime startTime;
+		private int durationInHours;
+		private Integer userCount;
+	}
+
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	@Getter
+	public static class AvailableRoomResponse {
+		private Long roomId;
+		private String roomName;
+		private RoomType roomType;
+		private RoomStatus roomStatus;
+		private Integer pricePerHour;
+		private String description;
+	}
+
+}
