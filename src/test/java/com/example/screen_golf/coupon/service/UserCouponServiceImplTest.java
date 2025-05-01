c11cb8abb732c2280d141c0aba6332269082fbd4
taewoocode <taewoocode@gmail.com>
2025-05-02 00:14:40 +0900

    GF-6[test]: 사용가능한 쿠폰 리스트 반환 테스트 작성

--- src/test/java/com/example/screen_golf/coupon/service/UserCouponServiceImplTest.java
+++ src/test/java/com/example/screen_golf/coupon/service/UserCouponServiceImplTest.java
@@ -1,25 +1,34 @@
 package com.example.screen_golf.coupon.service;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.example.screen_golf.coupon.domain.CouponStatus;
 import com.example.screen_golf.coupon.domain.UserCoupon;
+import com.example.screen_golf.coupon.dto.UserCouponListInfo;
 import com.example.screen_golf.coupon.repository.UserCouponRepository;
 import com.example.screen_golf.user.domain.User;
 import com.example.screen_golf.user.domain.UserRole;
 import com.example.screen_golf.user.domain.UserStatus;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class UserCouponServiceImplTest {
 
-	@Autowired
+	@Mock
 	private UserCouponRepository userCouponRepository;
+
+	@InjectMocks
+	private UserCouponServiceImpl userCouponService;
 
 	@Test
 	@DisplayName("사용자가_쿠폰을_보유하고_있으면_쿠폰_목록을_반환해야_한다")
@@ -57,9 +66,31 @@ class UserCouponServiceImplTest {
 		List<UserCoupon> userCoupons = testUser.getUserCoupons();
 
 		// Then
-		Assertions.assertThat(userCoupons)
+		assertThat(userCoupons)
 			.isNotNull()
 			.hasSize(2)
 			.contains(testCoupon1, testCoupon2);
 	}
+
+	@Test
+	@DisplayName("사용자의 사용 가능한 쿠폰 리스트를 반환한다.")
+	void 사용_가능한_쿠폰_리스트_반환() throws Exception {
+		Long userId = 1L;
+		UserCouponListInfo.UserCouponListRequest request = new UserCouponListInfo.UserCouponListRequest(userId);
+
+		LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+		LocalDateTime tomorrow = LocalDateTime.now().plusDays(1);
+		UserCoupon coupon = new UserCoupon(
+			null, "COUPON123", "Test Coupon", 100, yesterday, tomorrow);
+
+		when(userCouponRepository.findAvailableCoupons(eq(userId), eq(CouponStatus.UNUSED), any()))
+			.thenReturn(List.of(coupon));
+
+		List<UserCouponListInfo.UserCouponListResponse> result = userCouponService.getUserCouponsByUserId(request);
+
+		assertThat(result.get(0).getCouponCode()).isEqualTo("COUPON123");
+		assertThat(result.get(0).getName()).isEqualTo("Test Coupon");
+		assertThat(result.get(0).getDiscountAmount()).isEqualTo(100);
+	}
+
 }
