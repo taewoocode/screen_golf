0fb2e4934235f580888e23a614543bad1321d670
taewoocode <taewoocode@gmail.com>
2025-05-01 23:21:02 +0900

    GF-6[feat]: 유저가 가지고 있는 쿠폰리스트들을 반환하는 DTO

---
+++ src/main/java/com/example/screen_golf/coupon/dto/UserCouponListInfo.java
@@ -0,0 +1,47 @@
+package com.example.screen_golf.coupon.dto;
+
+import java.time.LocalDateTime;
+
+import com.example.screen_golf.coupon.domain.UserCoupon;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class UserCouponListInfo {
+
+	@Builder
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class UserCouponListRequest {
+		private Long userId;
+	}
+
+	@Builder
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class UserCouponListResponse {
+		private Long id;
+		private String couponCode;
+		private String name;
+		private Integer discountAmount;
+		private LocalDateTime validFrom;
+		private LocalDateTime validTo;
+		private String status;
+
+		public static UserCouponListResponse toDto(UserCoupon coupon) {
+			return UserCouponListResponse.builder()
+				.id(coupon.getId())
+				.couponCode(coupon.getCouponCode())
+				.name(coupon.getName())
+				.discountAmount(coupon.getDiscountAmount())
+				.validFrom(coupon.getValidFrom())
+				.validTo(coupon.getValidTo())
+				.status(coupon.getStatus().name()) // Enum을 String으로 반환
+				.build();
+		}
+	}
+}
