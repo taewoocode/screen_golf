7338089b7e99d3e869d14d7aa7111e3406595dd3
taewoocode <taewoocode@gmail.com>
2025-05-03 02:01:37 +0900

    GF-7[feat]: 할인 쿠폰 스케줄링 추가

--- src/main/java/com/example/screen_golf/ScreenGolfApplication.java
+++ src/main/java/com/example/screen_golf/ScreenGolfApplication.java
@@ -3,9 +3,11 @@ package com.example.screen_golf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class ScreenGolfApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenGolfApplication.class, args);
