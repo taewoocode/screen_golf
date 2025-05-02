b45b4467d3f5df93d8bc62b958bda57c992ad802
taewoocode <taewoocode@gmail.com>
2025-05-03 01:54:43 +0900

    GF-7[feat]: 룸 예외 룸 레포지토리 중복 제거

--- src/main/java/com/example/screen_golf/exception/reservation/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.screen_golf.exception.reservation;
-
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String message) {
-		super(message);
-	}
-}
