f7a166fea88afbb618f4053ad6523771a3d78579
taewoocode <taewoocode@gmail.com>
2025-05-03 02:04:21 +0900

    GF-7[feat]: 쿠폰 dto 변환 로직 추가

--- src/main/java/com/example/screen_golf/coupon/dto/UserCouponSearchUserIdInfo.java
+++ src/main/java/com/example/screen_golf/coupon/dto/UserCouponSearchUserIdInfo.java
@@ -2,7 +2,10 @@ package com.example.screen_golf.coupon.dto;
 
 import java.time.LocalDateTime;
 
+import com.example.screen_golf.coupon.domain.CouponPolicy;
 import com.example.screen_golf.coupon.domain.CouponStatus;
+import com.example.screen_golf.coupon.domain.UserCoupon;
+import com.example.screen_golf.user.domain.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,11 +26,28 @@ public class UserCouponSearchUserIdInfo {
 		private Long id;
 		private String couponCode;
 		private String name;
-		private Integer discountAmount;
+		private CouponPolicy couponPolicy;
 		private LocalDateTime validFrom;
 		private LocalDateTime validTo;
 		private CouponStatus status;
 		private LocalDateTime createdAt;
 		private LocalDateTime updatedAt;
+
+		public static UserCouponSearchCouponIdResponse toDto(User user, UserCoupon coupon, CouponPolicy policy) {
+			return UserCouponSearchUserIdInfo.UserCouponSearchCouponIdResponse.builder()
+				.userId(user.getId())
+				.id(coupon.getId())
+				.couponCode(coupon.getCouponCode())
+				.name(policy.name())
+				.couponPolicy(policy)
+				.validFrom(coupon.getValidFrom())
+				.validTo(coupon.getValidTo())
+				.status(coupon.getStatus())
+				.createdAt(coupon.getCreatedAt())
+				.updatedAt(coupon.getUpdatedAt())
+				.build();
+
+		}
 	}
+
 }
