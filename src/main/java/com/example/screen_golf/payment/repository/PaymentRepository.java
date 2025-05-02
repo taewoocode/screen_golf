7eaf12c21b138e71a29bee561b5f7c5b98acfecb
taewoocode <taewoocode@gmail.com>
2025-05-03 01:53:50 +0900

    GF-7[feat]: 결제에서 예약 찾기 추가

--- src/main/java/com/example/screen_golf/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/screen_golf/payment/repository/PaymentRepository.java
@@ -1,10 +1,18 @@
 package com.example.screen_golf.payment.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.screen_golf.payment.domain.Payment;
+import com.example.screen_golf.payment.domain.PaymentStatus;
+import com.example.screen_golf.reservation.domain.Reservation;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByReservation(Reservation reservation);
+    List<Payment> findByStatus(PaymentStatus status);
+    Optional<Payment> findById(Long id);
 }
