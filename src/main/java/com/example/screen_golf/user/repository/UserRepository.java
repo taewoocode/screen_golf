f9aa41d7481ec6519dad0451a281c18821f60b6d
taewoocode <taewoocode@gmail.com>
2025-05-03 02:05:18 +0900

    GF-7[feat]: 활성화 된 유저 제거

--- src/main/java/com/example/screen_golf/user/repository/UserRepository.java
+++ src/main/java/com/example/screen_golf/user/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.screen_golf.user.domain.User;
+import com.example.screen_golf.user.domain.UserStatus;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -20,6 +21,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	 * 활성화된 유저 조회
 	 * @return
 	 */
-	List<User> findAllByActiveTrue();
+	// List<User> findAllByActiveTrue();
+
+	List<User> findAllByStatus(UserStatus status);
 
 }
