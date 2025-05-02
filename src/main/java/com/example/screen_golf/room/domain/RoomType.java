1c85257c8eca1fdbef7faf4ad2cc29adabf5614d
taewoocode <taewoocode@gmail.com>
2025-05-03 02:01:06 +0900

    GF-7[feat]: 룸 상태 주석 추가

--- src/main/java/com/example/screen_golf/room/domain/RoomType.java
+++ src/main/java/com/example/screen_golf/room/domain/RoomType.java
@@ -1,7 +1,7 @@
 package com.example.screen_golf.room.domain;
 
 public enum RoomType {
-    STANDARD,    // 일반
-    PREMIUM,     // 프리미엄
-    VIP          // VIP
+    STANDARD,   // 일반 룸
+    PREMIUM,    // 프리미엄 룸
+    VIP         // VIP 룸
 } 
