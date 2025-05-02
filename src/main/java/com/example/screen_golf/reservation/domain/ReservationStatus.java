35472bbae8b6e01991330629566cb06cf923fb41
taewoocode <taewoocode@gmail.com>
2025-05-03 01:58:51 +0900

    GF-7[feat]: 예약 상태 주석 추가

--- src/main/java/com/example/screen_golf/reservation/domain/ReservationStatus.java
+++ src/main/java/com/example/screen_golf/reservation/domain/ReservationStatus.java
@@ -1,8 +1,8 @@
 package com.example.screen_golf.reservation.domain;
 
 public enum ReservationStatus {
-    PENDING,     // 대기
-    CONFIRMED,   // 확정
-    CANCELLED,   // 취소
-    COMPLETED    // 완료
+	PENDING,    // 대기
+	CONFIRMED,  // 확정
+	CANCELLED,  // 취소
+	COMPLETED   // 완료
 } 
