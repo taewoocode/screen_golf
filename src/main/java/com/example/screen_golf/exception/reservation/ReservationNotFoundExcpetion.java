8a03361a27eb137e4d55ceef72ce354b1279c916
taewoocode <taewoocode@gmail.com>
2025-05-03 01:51:14 +0900

    GF-7[feat]: 예약 예외처리 추가

---
+++ src/main/java/com/example/screen_golf/exception/reservation/ReservationNotFoundExcpetion.java
@@ -0,0 +1,7 @@
+package com.example.screen_golf.exception.reservation;
+
+public class ReservationNotFoundExcpetion extends RuntimeException {
+	public ReservationNotFoundExcpetion(String message) {
+		super(message);
+	}
+}
