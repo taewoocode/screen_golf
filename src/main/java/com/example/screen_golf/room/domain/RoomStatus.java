073a7a31ba8f74a7bbf48bb407ed885780aad607
taewoocode <taewoocode@gmail.com>
2025-05-03 02:00:51 +0900

    GF-7[feat]: 룸 상태 예약된 상태 추가

--- src/main/java/com/example/screen_golf/room/domain/RoomStatus.java
+++ src/main/java/com/example/screen_golf/room/domain/RoomStatus.java
@@ -1,8 +1,7 @@
 package com.example.screen_golf.room.domain;
 
 public enum RoomStatus {
-    AVAILABLE,    // 사용 가능
-    RESERVED,     // 예약됨
-    IN_USE,       // 사용 중
-    MAINTENANCE   // 점검 중
+	AVAILABLE,      // 사용 가능
+	MAINTENANCE,    // 점검 중
+	IN_USE, RESERVED        // 예약됨
 } 
