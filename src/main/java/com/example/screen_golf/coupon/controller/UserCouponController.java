3968cc88a1bca46794d311c89cdcb88f34688ab2
taewoocode <taewoocode@gmail.com>
2025-05-03 02:02:41 +0900

    GF-7[feat]: 할인 쿠폰 생성 api 주석처리 사용자가 직접생성 로직 제거

--- src/main/java/com/example/screen_golf/coupon/controller/UserCouponController.java
+++ src/main/java/com/example/screen_golf/coupon/controller/UserCouponController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.screen_golf.coupon.dto.UserCouponCreateInfo;
 import com.example.screen_golf.coupon.dto.UserCouponDeleteInfo;
 import com.example.screen_golf.coupon.dto.UserCouponListInfo;
 import com.example.screen_golf.coupon.dto.UserCouponSearchCouponIdInfo;
@@ -34,20 +33,20 @@ public class UserCouponController {
 
 	private final UserCouponService userCouponService;
 
-	@Operation(
-		summary = SwaggerDocs.SUMMARY_CREATE_USER_COUPON,
-		description = SwaggerDocs.DESCRIPTION_CREATE_USER_COUPON
-	)
-	@PostMapping("/create")
-	public ResponseEntity<UserCouponCreateInfo.UserCouponCreateResponse> createUserCoupon(
-		@Parameter(description = "발급할 쿠폰 정보", required = true)
-		@RequestBody UserCouponCreateInfo.UserCouponCreateRequest request
-	) {
-		UserCouponCreateInfo.UserCouponCreateResponse couponResponse
-			= userCouponService.createCoupon(request);
-		log.info("쿠폰 생성 성공 - 쿠폰 이름 = {}, 사용자 ID={}", couponResponse.getGetCouponName(), couponResponse.getUserId());
-		return ResponseEntity.ok(couponResponse);
-	}
+	// @Operation(
+	// 	summary = SwaggerDocs.SUMMARY_CREATE_USER_COUPON,
+	// 	description = SwaggerDocs.DESCRIPTION_CREATE_USER_COUPON
+	// )
+	// @PostMapping("/create")
+	// public ResponseEntity<UserCouponCreateInfo.UserCouponCreateResponse> createUserCoupon(
+	// 	@Parameter(description = "발급할 쿠폰 정보", required = true)
+	// 	@RequestBody UserCouponCreateInfo.UserCouponCreateRequest request
+	// ) {
+	// 	UserCouponCreateInfo.UserCouponCreateResponse couponResponse
+	// 		= userCouponService.createCoupon(request);
+	// 	log.info("쿠폰 생성 성공 - 쿠폰 이름 = {}, 사용자 ID={}", couponResponse.getGetCouponName(), couponResponse.getUserId());
+	// 	return ResponseEntity.ok(couponResponse);
+	// }
 
 	@Operation(
 		summary = SwaggerDocs.SUMMARY_DELETE_USER_COUPON,
