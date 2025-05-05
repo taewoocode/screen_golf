6261b4183107eaabba60db6afc13121bb2f05a40
taewoocode <taewoocode@gmail.com>
2025-05-05 20:29:08 +0900

    GF-8[feat]: 유저 엔티티 포인트 엔티티 추가

---
+++ src/main/java/com/example/screen_golf/point/Point.java
@@ -0,0 +1,58 @@
+package com.example.screen_golf.point;
+
+import java.time.LocalDateTime;
+
+import com.example.screen_golf.user.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "points")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Point {
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(nullable = false)
+	private Integer amount;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private PointType type;
+
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+
+	public Point(User user, Integer amount, PointType pointType, LocalDateTime createdAt) {
+		validateChargeUnit(amount);
+		this.user = user;
+		this.amount = amount;
+		this.type = pointType;
+		this.createdAt = createdAt;
+	}
+
+	private void validateChargeUnit(Integer amount) {
+		if (type == PointType.CHARGE && amount % 5000 != 0) {
+			throw new IllegalArgumentException("포인트는 5,000원 단위로만 충전할 수 있습니다.");
+		}
+	}
+}
