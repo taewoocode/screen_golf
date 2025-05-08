a7c051bbef8cf980254e49b211914a8ef2826e09
taewoocode <taewoocode@gmail.com>
2025-05-08 20:01:35 +0900

    GF-9[feat]: 게시글 타입 분리

--- src/main/java/com/example/screen_golf/community/domain/PostType.java
+++ src/main/java/com/example/screen_golf/community/domain/PostType.java
@@ -1,28 +1,28 @@
 package com.example.screen_golf.community.domain;
 
 public enum PostType {
-    NOTICE("공지사항"),
-    QUESTION("질문"),
-    GENERAL("일반"),
-    ADVERT("광고");
+	NOTICE("공지사항"),
+	QUESTION("질문"),
+	GENERAL("일반"),
+	ADVERT("광고");
 
-    private final String description;
+	private final String description;
 
-    PostType(String description) {
-        this.description = description;
-    }
+	PostType(String description) {
+		this.description = description;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    // 코드로부터 Enum 객체를 가져오는 메서드
-    public static PostType fromCode(String code) {
-        for (PostType type : values()) {
-            if (type.name().equalsIgnoreCase(code)) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Invalid post type code: " + code);
-    }
+	// 코드로부터 Enum 객체를 가져오는 메서드
+	public static PostType fromCode(String code) {
+		for (PostType type : values()) {
+			if (type.name().equalsIgnoreCase(code)) {
+				return type;
+			}
+		}
+		throw new IllegalArgumentException("Invalid board type code: " + code);
+	}
 } 
