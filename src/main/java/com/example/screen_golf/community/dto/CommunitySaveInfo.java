82470ef6051b7ac3aecfbf7fb726d2d2594c3d7b
taewoocode <taewoocode@gmail.com>
2025-05-08 19:59:27 +0900

    GF-9[feat]: 커뮤니티 게시글 작성 dto 수정

--- src/main/java/com/example/screen_golf/community/dto/CommunitySaveInfo.java
+++ src/main/java/com/example/screen_golf/community/dto/CommunitySaveInfo.java
@@ -1,6 +1,7 @@
 package com.example.screen_golf.community.dto;
 
 import com.example.screen_golf.community.domain.PostType;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
