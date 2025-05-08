af5e67b4bbda305394792830b7ad3bc02a911b19
taewoocode <taewoocode@gmail.com>
2025-05-08 19:59:45 +0900

    GF-9[feat]: 커뮤니티 게시글 작성 기능 추가

--- src/main/java/com/example/screen_golf/community/service/CommunityServiceImpl.java
+++ src/main/java/com/example/screen_golf/community/service/CommunityServiceImpl.java
@@ -1,4 +1,35 @@
 package com.example.screen_golf.community.service;
 
-public class CommunityServiceImpl {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.screen_golf.community.domain.Community;
+import com.example.screen_golf.community.dto.CommunityConverter;
+import com.example.screen_golf.community.dto.CommunitySaveInfo;
+import com.example.screen_golf.community.repository.elasticsearch.CommunitySearchRepository;
+import com.example.screen_golf.community.repository.jpa.CommunityRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CommunityServiceImpl implements CommunityService {
+
+	private final CommunityRepository communityRepository;
+	private final CommunitySearchRepository communitySearchRepository;
+
+	/**
+	 *
+	 * @param request
+	 * @return
+	 */
+	@Override
+	@Transactional
+	public CommunitySaveInfo.CommunitySaveResponse savePost(CommunitySaveInfo.CommunitySaveRequest request) {
+		Community entity = CommunityConverter.toEntity(request);
+		Community savedPost = communityRepository.save(entity);
+		return CommunityConverter.toResponse(savedPost);
+	}
 }
