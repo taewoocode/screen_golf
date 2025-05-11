c3428da2ddd04d669593102a49c17312c5621990
taewoocode <taewoocode@gmail.com>
2025-05-12 03:30:49 +0900

    GF-10[feat]: 스웨거 토큰 기능 추가

--- src/main/java/com/example/screen_golf/config/SwaggerConfig.java
+++ src/main/java/com/example/screen_golf/config/SwaggerConfig.java
@@ -3,9 +3,12 @@ package com.example.screen_golf.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -21,6 +24,17 @@ public class SwaggerConfig {
 					.name("Apache 2.0")
 					.url("https://www.apache.org/licenses/LICENSE-2.0")
 				)
+			)
+			.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+			.components(new Components()
+				.addSecuritySchemes("Bearer Authentication", createAPIKeyScheme())
 			);
+	}
+
+	private SecurityScheme createAPIKeyScheme() {
+		return new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.bearerFormat("JWT")
+			.scheme("bearer");
 	}
 }
