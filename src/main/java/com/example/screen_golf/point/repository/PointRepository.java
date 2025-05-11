aa9f350bfebfa800c5d194a2d33e799db5658535
taewoocode <taewoocode@gmail.com>
2025-05-12 03:27:09 +0900

    GF-10[feat]: 유저의 포인트 조회 쿼리 추가

--- src/main/java/com/example/screen_golf/point/repository/PointRepository.java
+++ src/main/java/com/example/screen_golf/point/repository/PointRepository.java
@@ -1,8 +1,16 @@
 package com.example.screen_golf.point.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.screen_golf.point.domain.Point;
 
 public interface PointRepository extends JpaRepository<Point, Long> {
+	@Query("SELECT SUM(p.amount) FROM Point p WHERE p.user.id = :userId")
+	Integer sumAmountByUserId(@Param("userId") Long userId);
+
+	List<Point> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
